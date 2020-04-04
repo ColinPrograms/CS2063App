834d8557de377a8919072e73cdab528148256715
lpepin1 <lpepin@unb.ca>
2020-04-04 16:48:32 -0300

    CHECKING LOCATION IN MAIN ACTIVITY

--- app/src/main/java/com/mobile/app/LocationFinder.java
+++ app/src/main/java/com/mobile/app/LocationFinder.java
@@ -1,22 +1,52 @@
 package com.mobile.app;
 
+import android.database.Cursor;
 import android.location.Location;
 
 import java.util.ArrayList;
 
 public class LocationFinder {
 
-    public static String findPlaylist(ArrayList<PlaylistObject> playlists){
-        return nearestBubble(playlists);
+    public static ArrayList<PlaylistObject> createPlaylistArrayList(DatabaseHelper mydb, Location location){
+       // DatabaseHelper mydb = new DatabaseHelper(this);
+        ArrayList<PlaylistObject> playlistObjects = new ArrayList<PlaylistObject>();
+        Cursor cursor = mydb.getTableRows("playlists_table");
+
+        if(cursor != null && cursor.moveToFirst()) {
+
+            String playlistName = cursor.getString(1);
+            String[] playlistLocation = cursor.getString(2).split(" ");
+            PlaylistObject newPlaylist = new PlaylistObject(playlistName, Double.valueOf(playlistLocation[0]), Double.valueOf(playlistLocation[1]) ,Double.valueOf(playlistLocation[2]));
+
+            playlistObjects.add(newPlaylist);
+
+            while(cursor.moveToNext()){
+                
+                playlistName = cursor.getString(1);
+                playlistLocation = cursor.getString(2).split(" ");
+                newPlaylist = new PlaylistObject(playlistName, Double.valueOf(playlistLocation[0]), Double.valueOf(playlistLocation[1]) ,Double.valueOf(playlistLocation[2]));
+
+                playlistObjects.add(newPlaylist);
+            }
+        }
+
+        return playlistObjects;
+    }
+    public static String findPlaylist(DatabaseHelper mydb, Location location){
+        return nearestBubble(createPlaylistArrayList(mydb, location), location);
     }
 
-    private static String nearestBubble(ArrayList<PlaylistObject> playlists){
+    private static String nearestBubble(ArrayList<PlaylistObject> playlists, Location location){
 
         PlaylistObject closestPlaylist = playlists.get(0);
         double smallestDistance = 1000000000;
+        float results[] = new float[10];
         for(int i = 0; i < playlists.size(); i++){
-            //double distance = Location.distanceBetween(playlists.get(i).getLat(), playlists.get(i).getLng()) ;
-            double distance = 10;
+
+            Location.distanceBetween(playlists.get(i).getLat(), playlists.get(i).getLng(), location.getLatitude(), location.getLongitude(),results);
+
+            double distance = results[0];
+
             distance -= playlists.get(i).getRad();
             if(distance < smallestDistance){
                 smallestDistance = distance;
--- app/src/main/java/com/mobile/app/MainActivity.java
+++ app/src/main/java/com/mobile/app/MainActivity.java
@@ -2,21 +2,37 @@ package com.mobile.app;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements LocationListener {
     MusicService mService;
     boolean bound = false;
+
+    LocationManager locationManager;
+    boolean isGpsLocation;
+    Location loc;
+    boolean isNetworklocation;
+
+
+    private static final int PERMISSION_CODE = 101;
+    String[] permissions_all={Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }
+        if(!checkPermission()){
+            requestPermission();
         }
     }
 
@@ -56,6 +75,7 @@ public class MainActivity extends Activity {
     }
 
     public void playbtnClick(View v) {
+
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
             if(!bound){
                 Intent musicServiceIntent = new Intent(this, MusicService.class);
@@ -71,6 +91,114 @@ public class MainActivity extends Activity {
         }else{
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
         }
+
+
+        if(checkPermission()){
+            getLocation();
+            Toast.makeText(this, loc.getLatitude() + " " + loc.getLongitude(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(MainActivity.this,permissions_all,PERMISSION_CODE);
+    }
+
+    private boolean checkPermission() {
+        for(int i=0;i<permissions_all.length;i++){
+            int result= ContextCompat.checkSelfPermission(MainActivity.this,permissions_all[i]);
+            if(result== PackageManager.PERMISSION_GRANTED){
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void getLocation() {
+            if(checkPermission()){
+                getDeviceLocation();
+            }
+            else{
+                requestPermission();
+            }
+    }
+
+
+
+    private void getDeviceLocation() {
+        //now all permission part complete now let's fetch location
+        locationManager=(LocationManager)getSystemService(Service.LOCATION_SERVICE);
+        isGpsLocation=locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        isNetworklocation=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if(!isGpsLocation && !isNetworklocation){
+            // showSettingForLocation();
+            getLastlocation();
+        }
+        else{
+            getFinalLocation();
+        }
+    }
+
+    private void getLastlocation() {
+        if(locationManager!=null) {
+            try {
+                Criteria criteria = new Criteria();
+                String provider = locationManager.getBestProvider(criteria,false);
+                Location location=locationManager.getLastKnownLocation(provider);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void getFinalLocation() {
+        try{
+            if(isGpsLocation){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000*60*1,10,this);
+                if(locationManager!=null){
+                    loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if(loc!=null){
+                        this.loc = loc;
+                    }
+                }
+            }
+            else if(isNetworklocation){
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60*1,10,MainActivity.this);
+                if(locationManager!=null){
+                    loc=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if(loc!=null){
+                        this.loc = loc;
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this, "Can't Get Location", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch (SecurityException e){
+            Toast.makeText(this, "Can't Get Location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 }
