8c429eacb9177b863ce08ca6b7d0b26812d11487
ColinPrograms <w6c0j@unb.ca>
2020-04-05 15:02:49 -0300

    plays next song correctly

--- app/src/main/java/com/mobile/app/MusicService.java
+++ app/src/main/java/com/mobile/app/MusicService.java
@@ -7,7 +7,6 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
 
 import java.io.IOException;
 
@@ -43,15 +42,16 @@ public class MusicService extends Service {
     int count = 0;
     public void play(){
         Cursor cursor = mydb.getTableRows("playlist1");
+        int size = cursor.getCount();
+        if(count>=size){
+            count = 0;
+        }
         if(!playing){
-            int size = cursor.getCount();
-            if(count>=size){
-                count = 0;
-            }
             cursor.moveToPosition(count);
             String uriS = cursor.getString(3);
             Uri uri = Uri.parse(uriS);
             try{
+                player.reset();
                 player.setDataSource(this,uri);
                 player.prepareAsync();
             }catch(IOException e){
@@ -62,18 +62,17 @@ public class MusicService extends Service {
                 public void onPrepared(MediaPlayer mp) {
                     mp.start();
                     count++;
-                    Log.e("serviceee",count +"prepared");
                     playing = true;
                 }
                 });
         }else{
-            Log.e("serviceee",count +"else");
             player.start();
         }
 
         player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
+                playing = false;
                 play();
             }
         });
